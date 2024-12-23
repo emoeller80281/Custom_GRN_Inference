1e595d01a2a7c94798ae76c4a53d6ef86f013a89
emoeller <luminarada80@gmail.com>
2024-12-23 16:36:15 -0500

    The main issue I am having is the small amount of overlapping TFs with the genes in the RNA dataset

--- src/python_scripts/find_overlapping_TFs.py
+++ src/python_scripts/find_overlapping_TFs.py
@@ -13,6 +13,14 @@ TF_motif_binding_df = pd.read_csv(TF_motif_binding_file, header=0, sep="\t", ind
 # Find overlapping TFs
 RNA_dataset = RNA_dataset.rename(columns={RNA_dataset.columns[0]: "Genes"})
 
+print(f'Size of unfiltered RNA dataset:')
+print(f'\tGenes: {RNA_dataset.shape[1]}')
+print(f'\tCells: {RNA_dataset.shape[0]}')
+
+print(f'\nSize of unfiltered TF motif dataset:')
+print(f'\tTFs: {len(set(TF_motif_binding_df["Source"]))}')
+print(f'\tTGs: {len(TF_motif_binding_df["Target"])}')
+
 genes = set(RNA_dataset["Genes"])
 
 overlapping_TF_motif_binding_df = TF_motif_binding_df[
@@ -25,11 +33,26 @@ aligned_RNA = RNA_dataset[RNA_dataset["Genes"].isin(overlapping_TF_motif_binding
 
 RNA_expression_matrix = RNA_dataset.iloc[1:, 1:].values
 
-threshold = np.mean(RNA_expression_matrix) * np.std(RNA_expression_matrix)
-print(f'Threshold = {threshold}')
+print(f'Mean RNA expression value = {np.mean(RNA_expression_matrix)}')
+
+expr_above_zero = RNA_expression_matrix[RNA_expression_matrix > 0]
+
+threshold = np.percentile(expr_above_zero, 10)  # Set to the 10th percentile
+print(f"10th Percentile Threshold = {threshold}")
 
 row_sums = RNA_dataset.iloc[1:, 1:].sum(axis=1)  # Compute row sums for filtering
 filtered_genes = RNA_dataset.iloc[1:, :][row_sums > threshold]  # Use the same row index
+
+plt.figure(figsize=(10, 6))
+
+filtered_expr_values = filtered_genes.iloc[:, 1:].values.flatten()
+filtered_expr_values = filtered_expr_values[filtered_expr_values > 0]
+
+plt.hist(np.log10(filtered_expr_values), bins=50, color="blue", alpha=0.7, log=True)
+plt.title("Distribution of Expression Values")
+plt.xlabel("Log10 Expression Value")
+plt.ylabel("Log10 Frequency")
+plt.savefig("RNA_expression_distribution.png", dpi=200)
 
 
 # Align overlapping_TF_motif_binding_df to the filtered RNA dataset
@@ -82,4 +105,4 @@ plt.xlabel("Expression Value")
 plt.ylabel("Frequency")
 plt.savefig("TF_score_distribution.png", dpi=200)
 
-print(f'Mean gene score = {weighted_expression_df.mean()}')
+# print(f'Mean gene score = {weighted_expression_df.mean()}')
