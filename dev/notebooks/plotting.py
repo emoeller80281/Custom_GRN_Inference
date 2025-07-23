c23d610a00a2f7e7c883d32c0a20e9218b25ed1b
emoeller <luminarada80@gmail.com>
2025-07-23 12:01:30 -0400

    Added assertions to dev plots

--- dev/notebooks/plotting.py
+++ dev/notebooks/plotting.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import math
-import random
+import numpy as np
 from typing import Union
 
 def balance_dataset(
@@ -39,51 +39,68 @@ def plot_scores_grouped_by_tf_tg(df, title, score_col):
 def plot_score_distribution_by_tf(
     df: pd.DataFrame,
     feature_col: str,
-    tfs_of_interest: list[str],
-    limit_x: bool = True,
-    top_tf_limit: int = 40
+    tf_col_name: str = "source_id",
+    tfs_of_interest: list[str] = [],
+    title: str = "",
+    limit_x: bool = False,
+    top_tf_limit: int = 40,
 ):
     fig = plt.figure(figsize=(7, 5))
     y_cmap = plt.get_cmap("tab20c")
     
+    assert tf_col_name in df.columns, f"tf_col_name {tf_col_name} not in df.columns {df.columns}"
+    
+    if len(tfs_of_interest) == 0:
+        print(f"No TFs specified by tfs_of_interest, plotting top top_tf_limit ({top_tf_limit}) TFs")
+        tfs_of_interest = df[tf_col_name].drop_duplicates().to_list()
+        
+    assert all(x in df[tf_col_name].to_list() for x in tfs_of_interest), \
+        f"Found items in tfs_of_interest not in df: {[i for i in tfs_of_interest if i not in df[tf_col_name].to_list()]}"
+        
     # Limit the number of TFs to plot
     if len(tfs_of_interest) > top_tf_limit:
         print(f"Limiting to scores from the top {top_tf_limit} TFs, found {len(tfs_of_interest)}")
-        tf_rows_in_df = df[df["source_id"].isin(tfs_of_interest)]
+        tf_rows_in_df = df[df[tf_col_name].isin(tfs_of_interest)]
         df_by_tf = (
             tf_rows_in_df
-            .groupby("source_id")
+            .groupby(tf_col_name)
             .count()
             .sort_values(feature_col, ascending=False)
             .reset_index()
             .iloc[:top_tf_limit]
             )
-        tfs_of_interest = df_by_tf["source_id"].drop_duplicates().to_list()
+        tfs_of_interest = df_by_tf[tf_col_name].drop_duplicates().to_list()
         
     # Orders by the TF with the most scores -> least scores
     else:
-        tf_rows_in_df = df[df["source_id"].isin(tfs_of_interest)]
+        tf_rows_in_df = df[df[tf_col_name].isin(tfs_of_interest)]
         df_by_tf = (
             tf_rows_in_df
-            .groupby("source_id")
+            .groupby(tf_col_name)
             .count()
             .sort_values(feature_col, ascending=False)
             .reset_index()
             )
-        tfs_of_interest = df_by_tf["source_id"].drop_duplicates().to_list()
-        
+        tfs_of_interest = df_by_tf[tf_col_name].drop_duplicates().to_list()
+    
+    # Sets the bin widths to be equal for all TF score distributions
+    scores = df[feature_col].dropna()
+    min_score = scores.min()
+    max_score = scores.max()
+    bin_width = max((max_score - min_score) / 85, 1e-3)
+    bins = np.arange(min_score, max_score + bin_width, bin_width).tolist()
+    
     for x, tf in enumerate(tfs_of_interest):
-        tfs = df[df["source_id"] == tf]
-        percent_total_true_edges = len(tfs[feature_col]) / len(df[feature_col])
-        nbins=max(30, math.ceil(50*percent_total_true_edges))
+        tfs = df[df[tf_col_name] == tf]
         plt.hist(
             tfs[feature_col].dropna(),
-            bins=nbins, alpha=0.5,
+            bins=bins, alpha=0.5,
             color=y_cmap(x / len(tfs_of_interest)),
-            label=tf
+            label=tf,
         )
+
     # set titles/labels on the same ax
-    plt.title("Sliding window score distribution colored by TF", fontsize=12)
+    plt.title(title, fontsize=12)
     plt.xlabel("Sliding Window Score", fontsize=12)
     plt.ylabel("Frequency", fontsize=12)
     if limit_x:
@@ -102,14 +119,22 @@ def plot_true_false_distribution(
     true_series: pd.Series, 
     false_series: pd.Series, 
     xlabel: str = "Score", 
-    ylabel: str = "Frequency", 
+    ylabel: str = "", 
     title: str = "", 
     log: bool = False,
-    balance: bool = False
+    balance: bool = False,
+    density: bool = False
     ):
     
-    if balance == True:
+    if balance and not density: # Don't balance if using the density plot, it normalizes
         true_series, false_series = balance_dataset(true_series, false_series)
+    
+    if balance and density:
+        print("INFO: Setting density=True balances the dataset, balance can be set to False")
+    
+    if not balance and not density:
+        if len(true_series) > 10*len(false_series) or len(false_series) > 10*len(true_series):
+            print("WARNING: Number of score are unbalanced, consider setting balance=True or density=True")
     
     plt.figure(figsize=(6,4))
     plt.hist(
@@ -118,6 +143,7 @@ def plot_true_false_distribution(
         alpha=0.5,
         color="#4195df",
         label="Edge in\nGround Truth",
+        density=density,
         log=log
     )
     plt.hist(
@@ -126,12 +152,21 @@ def plot_true_false_distribution(
         alpha=0.5,
         color="#747474",
         label="Edge not in\nGround Truth",
+        density=density,
         log=log
     )
+    
+    if len(ylabel) == 0:
+        if not density:
+            ylabel = "Frequency"
+        if density:
+            ylabel = "Density"
+            
+    
     plt.title(title, fontsize=12)
     plt.xlabel(xlabel, fontsize=11)
     plt.ylabel(ylabel, fontsize=11)
-    plt.xticks(fontsize=10)
+    plt.xticks(fontsize=10, rotation=20)
     plt.yticks(fontsize=10)
     plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0., fontsize=10)
     plt.tight_layout()
