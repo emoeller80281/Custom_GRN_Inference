af181971418c53ec49df72a6098db3c6126be55e
emoeller <luminarada80@gmail.com>
2026-03-09 18:34:33 -0400

    Working on the single-cell RNA-seq and ATAC-seq integration pipeline from Muon. The LINGER preprocessing pipeline treats the RNA and ATAC the same, but I should do ATAC-specific preprocessing and QC filtering

--- dev/run_generate_batch_grad_attrib_and_tf_ko.sh
+++ dev/run_generate_batch_grad_attrib_and_tf_ko.sh
@@ -9,7 +9,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH -c 8
 #SBATCH --mem=64G
-#SBATCH --array=0-9%3
+#SBATCH --array=0-3%2
 
 set -eo pipefail
 
@@ -130,17 +130,17 @@ EXPERIMENT_LIST=(
     # "mESC_E8.0_rep1_hvg_filter_disp_0.4|model_training_001|mESC|E8.0_rep1"
     # "mESC_E8.0_rep1_hvg_filter_disp_0.3|model_training_001|mESC|E8.0_rep1"
     # "mESC_E8.0_rep1_hvg_filter_disp_0.2|model_training_001|mESC|E8.0_rep1"
-    "mESC_E8.0_rep1_hvg_filter_disp_0.1|model_training_001|mESC|E8.0_rep1"
-    "mESC_E8.0_rep1_hvg_filter_disp_0.05|model_training_001|mESC|E8.0_rep1"
-    "mESC_E8.0_rep1_hvg_filter_disp_0.01|model_training_001|mESC|E8.0_rep1"
+    # "mESC_E8.0_rep1_hvg_filter_disp_0.1|model_training_001|mESC|E8.0_rep1"
+    # "mESC_E8.0_rep1_hvg_filter_disp_0.05|model_training_001|mESC|E8.0_rep1"
+    # "mESC_E8.0_rep1_hvg_filter_disp_0.01|model_training_001|mESC|E8.0_rep1"
 
-    "mESC_E8.0_rep2_hvg_filter_only_rna|model_training_001|mESC|E8.0_rep2"
-    "mESC_E8.0_rep2_hvg_filter_disp_0.6|model_training_001|mESC|E8.0_rep2"
-    "mESC_E8.0_rep2_hvg_filter_disp_0.5|model_training_001|mESC|E8.0_rep2"
-    "mESC_E8.0_rep2_hvg_filter_disp_0.4|model_training_001|mESC|E8.0_rep2"
-    "mESC_E8.0_rep2_hvg_filter_disp_0.3|model_training_001|mESC|E8.0_rep2"
-    "mESC_E8.0_rep2_hvg_filter_disp_0.2|model_training_001|mESC|E8.0_rep2"
-    "mESC_E8.0_rep2_hvg_filter_disp_0.1|model_training_001|mESC|E8.0_rep2"
+    # "mESC_E8.0_rep2_hvg_filter_only_rna|model_training_001|mESC|E8.0_rep2"
+    # "mESC_E8.0_rep2_hvg_filter_disp_0.6|model_training_001|mESC|E8.0_rep2"
+    # "mESC_E8.0_rep2_hvg_filter_disp_0.5|model_training_001|mESC|E8.0_rep2"
+    # "mESC_E8.0_rep2_hvg_filter_disp_0.4|model_training_001|mESC|E8.0_rep2"
+    # "mESC_E8.0_rep2_hvg_filter_disp_0.3|model_training_001|mESC|E8.0_rep2"
+    # "mESC_E8.0_rep2_hvg_filter_disp_0.2|model_training_001|mESC|E8.0_rep2"
+    # "mESC_E8.0_rep2_hvg_filter_disp_0.1|model_training_001|mESC|E8.0_rep2"
     # "mESC_E8.0_rep2_hvg_filter_disp_0.05|model_training_001|mESC|E8.0_rep2"
     # "mESC_E8.0_rep2_hvg_filter_disp_0.01|model_training_001|mESC|E8.0_rep2"
 
@@ -176,6 +176,13 @@ EXPERIMENT_LIST=(
     # "mESC_E8.5_rep1_disp_0.2_192d|model_training_001|mESC|E8.5_rep1"
     # "mESC_E8.5_rep2_disp_0.2_192d|model_training_001|mESC|E8.5_rep2"
 
+    "mESC_E7.5_rep1_disp_0.2_256d|model_training_001|mESC|E7.5_rep1"
+    "mESC_E7.5_rep2_disp_0.2_256d|model_training_001|mESC|E7.5_rep2"
+    "mESC_E8.5_rep1_disp_0.2_256d|model_training_001|mESC|E8.5_rep1"
+    "mESC_E8.5_rep2_disp_0.2_256d|model_training_001|mESC|E8.5_rep2"
+
+    # "mESC_all_sample_disp_0.6|model_training_001|mESC|E7.5_rep1 E7.5_rep2 E8.5_rep1 E8.5_rep2"
+    # "mESC_preprocessing_testing|model_training_001|mESC|E7.5_rep1"
 )
 
 echo "Host: $(hostname)"
